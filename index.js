64ff2be7d94494cee9d52b5d3d4e060db1332af8
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2025-12-01 20:44:01 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+let animalName = string;
+
+let height = numeric;
+
+let isIindoorPet = boolean;
+
+
+
+document.getElementById("string").innerHTML = typeof animalName;
+document.getElementById("numeric").innerHTML = typeof height;
+document.getElementById("boolean").innerHTML = isIndoorPet;
